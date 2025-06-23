39b5f78e6e62880a43a9ef8e5a8b86a72fd6d065
Nepomunezero <j.munezero1@alustudent.com>
2025-06-23 05:17:02 -0700

    testing accomplished

--- python-hello_world/test.py
+++ python-hello_world/test.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 
-while True:
-    try:
-        message = input("Enter a number: ")
-        number = int(message)
-        for i in range(1, number, 1):
-            print(f"{i}*{number} ={i * number}")
-    except ValueError:
-        print("please enter entegers only")
+password = ""
+timesIntered = 1
+length = 0
+while password != "secret123":
+    password = input("Enter password: ")
+    for i in password:
+        length = length +1
+    if password == "secret123":
+        print("Access granted.")
+    elif timesIntered % 3 == 0:
+        print("Retry alert you seem to not remember your own password")
+        timesIntered = timesIntered + 1
+    elif length < 8:
+        print("Error Too short.")
+    elif not any(c.isdigit() for c in password):
+        print("Error: Need a digit.")
+        timesIntered = timesIntered + 1
+    else:
+        print("Access denied.")
+        timesIntered = timesIntered + 1
