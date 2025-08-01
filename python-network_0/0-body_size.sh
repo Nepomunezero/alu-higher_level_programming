c89ddbaadd1c0ec6a04cad1f7b6d0e2ba887011e
Nepomunezero <j.munezero1@alustudent.com>
2025-08-01 08:08:10 +0200

    updated file

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-# Takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes
-
-curl -s "$1" | wc -c
+#!/bin/bash 
+# take in a url and display the content 
+curl -sI "$1" | grep 'Content-Length'| cut -d " " -f2
